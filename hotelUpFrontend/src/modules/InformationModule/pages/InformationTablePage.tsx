0c9c314a307c57f1ea6439b481d43ed9e8b723eb
akopij70 <akopij70@gmail.com>
2025-01-13 18:42:45 +0100

    Mocked demo information table

--- hotelUpFrontend/src/modules/InformationModule/pages/InformationTablePage.tsx
+++ hotelUpFrontend/src/modules/InformationModule/pages/InformationTablePage.tsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import foodImage from "../../../assets/images/food.jpg";
+import hotelImage from "../../../assets/images/hotel.jpg";
+import './informationTable.css'
+
 function InformationTablePage() {
-  return (<>
-    <h1>Witamy!</h1>
-  </>);
+  const images = [hotelImage, foodImage];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const texts = [
+    {
+      title: "Dostępność pokoi",
+      description: "Aktualnie mamy dostępne 100 pokoi hotelowych",
+    },
+    {
+      title: "Szef kuchni poleca",
+      description: "Nasze menu oferuje szeroki wybór dań przygotowanych z najlepszych składników. Przyjdź i spróbuj wyjątkowych smaków naszych potraw.",
+    },
+  ];
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextImage();
+    }, 7000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+  return (
+    <div className="advert-container">
+      <div className="image-container">
+        <img src={images[currentIndex]} alt="Image" className="image" />
+        <div className="arrow-buttons">
+          <button className="change-button" onClick={prevImage}>&lt;</button>
+          <button className="change-button" onClick={nextImage}>&gt;</button>
+        </div>
+      </div>
+      <div className="text-container">
+        <h2>{texts[currentIndex].title}</h2>
+        <p>{texts[currentIndex].description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default InformationTablePage;
