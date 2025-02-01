873834ff6310b09d4d112041755559f5961c8a79
Wiaz <wiazhub@gmail.com>
2025-02-01 11:08:10 +0100

    cleanup

--- k8s/create-secrets.sh
+++ k8s/create-secrets.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ]; then
     NAMESPACE="hotelup"
 fi
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+profile="default"
 
 cat << EOF | kubectl apply -f -
     apiVersion: v1
@@ -14,10 +15,10 @@ cat << EOF | kubectl apply -f -
         namespace: ${NAMESPACE}
     type: Opaque
     data:
-        COGNITO_AUTHORITY: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/CognitoAuthority --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
-        COGNITO_CLIENT: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/ClientId --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
-        COGNITO_DOMAIN: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/CognitoDomain --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
-        BACKEND_URL: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/BackendUrl --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
-        LOGOUT_URI: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/LogoutUrl --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
-        REDIRECT_URI: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/RedirectUrl --with-decrypt --output text --profile wiaz --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        COGNITO_AUTHORITY: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/CognitoAuthority --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        COGNITO_CLIENT: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/ClientId --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        COGNITO_DOMAIN: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/Oidc/CognitoDomain --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        BACKEND_URL: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/BackendUrl --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        LOGOUT_URI: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/LogoutUrl --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
+        REDIRECT_URI: $(echo -n "`aws ssm get-parameter --name /HotelUp.Frontend/Production/RedirectUrl --with-decrypt --output text --profile $profile --region us-east-1 --query Parameter.Value`" | base64 -w0)
 EOF
